cb7aed9393fc5f97c2fc65e0d9bda810b4a63d67
Bella Cruz <bella.v.cruz@ou.edu>
2023-02-13 11:43:50 -0700

    added main and tested program

--- myCalculator.java
+++ myCalculator.java
@@ -1,6 +1,10 @@
-public assignmentThree{
+import java.util.Scanner;
+import java.util.Arrays;
 
-	public int add(int [] myList){
+
+public class myCalculator{
+
+	public static int add(int [] myList){
 
 		int sum = 0;
 
@@ -14,12 +18,12 @@ public assignmentThree{
 		return sum;
 	}
 
-	public int multiply(int [] myList){
+	public static int multiply(int [] myList, int count){
 
 		int product = 1;
 
-			for(int i = 0; i < myList.length; i++){
-
+			for(int i = 0; i < count; i++){
+				
 				product = product * myList[i];
 
 			}
@@ -30,6 +34,26 @@ public assignmentThree{
 
 	}
 
+	public static void main(String args[]){
 
+			Scanner stdin = new Scanner(System.in); 
+			int nextInput = stdin.nextInt();
+			int[] newList = new int[50]; 
+			int count = 0;
+
+			while(nextInput != -1){
+
+				newList[count] = nextInput;
+				count++;
+				nextInput = stdin.nextInt();
+			}
+
+			int mySum = add(newList);
+			int myProduct = multiply(newList, count);
+
+			System.out.println("The sum was " + mySum);
+			System.out.println("The product was " + myProduct); 
+	}
 
 }
+
