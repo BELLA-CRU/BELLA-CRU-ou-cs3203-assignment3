6421ce9abd5fe79b4ace764d1d48ea016c39ac3c
Bella Cruz <bella.v.cruz@ou.edu>
2023-02-13 11:12:59 -0700

    add method

--- myCalculator.java
+++ myCalculator.java
@@ -1,1 +1,19 @@
+public assignmentThree{
 
+	public int add(int [] myList){
+
+		int sum = 0;
+
+		for(int i = 0; i < myList.length; i++){
+
+			sum += myList[i];
+
+		}
+
+
+		return sum;
+	}
+
+
+
+}
